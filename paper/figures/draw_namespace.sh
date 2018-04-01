64ce8377303f4446ac2569e183326c9a0fd823d6
Michael Sevilla <mikesevilla3@gmail.com>
2018-03-31 19:54:56 -0700

    background: add section and describe repository
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

---
+++ paper/figures/draw_namespace.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+docker stop dot >> /dev/null 2>&1
+docker rm dot   >> /dev/null 2>&1
+set -x
+
+docker run -it --rm \
+  --net host \
+  --name dot \
+  -v /dev:/dev \
+  -v `pwd`:/root \
+  -w /root \
+  --privileged \
+  --entrypoint=/bin/bash \
+  michaelsevilla/plfs \
+  -c "dot -Tpng -o output.png input.dot"
+
