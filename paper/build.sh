2a30732fcc6010aafe5f9de65f453fecaf599267
Michael Sevilla <mikesevilla3@gmail.com>
2018-03-31 17:45:41 -0700

    paper: initial commit
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

---
+++ paper/build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+rm -r _minted-paper paper.aux paper.bbl paper.blg paper.log paper.out paper.synctex.gz build.log >> /dev/null 2>&1
+set -x
+cp biblio/references.bib paper.bib
+
+# Build the paper
+docker run --rm \
+  --name latex \
+  --entrypoint=/bin/bash \
+  -v `pwd`/:/mnt \
+  michaelsevilla/texlive:latest -c \
+    "cd /mnt ; \
+     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper; \
+     bibtex paper; \
+     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper; \
+     pdflatex -synctex=1 -interaction=nonstopmode -shell-escape paper;" &> build.log
+
+ERR=$?
+if [ $ERR != "0" ] ; then
+  set +x
+  echo "ERROR: $ERR"
+  cat build.log
+  exit 1
+fi
+
+set +x
+rm -r _minted-paper paper.aux paper.bbl paper.blg paper.log paper.out paper.synctex.gz build.log >> /dev/null 2>&1
+
+echo "SUCCESS"
+
